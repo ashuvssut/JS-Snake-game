ec2a4d7b8f3c7f7b9a38c97a915f94a968d5d3ac
ashuvssut <ashu.khanduala@gmail.com>
2020-06-06 23:56:45 +0530

    initial commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,24 @@ export const snakeSpeed = 1;       //blocks covered per sec
 const snakeBody = [{x: 11, y: 11}];// initial pos given
                                 //This array store bodyPieces. each body piece has a x and a y coordinate value
 
+export function logic(){
+    
+}
+
 export function update(){
-    console.log("update");
+    for()
 }
 
 export function draw(gameBoard){
     snakeBody.forEach(bodyPiece => {
+        //Prepare the snake body pieces...basically prepare a div to make it a grid that represents the bodyPiece and then just add it to the grid(gameBoard) as a child
         const snakeBodyPiece = document.createElement('div')
         snakeBodyPiece.style.gridRowStart = bodyPiece.x;
         snakeBodyPiece.style.gridColumnStart = bodyPiece.y;
+        snakeBodyPiece.classList.add('snake');
+        //set it as child of the grid base (gameBoard)
+        gameBoard.appendChild(snakeBodyPiece);
     });
+
+
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,37 +2,45 @@
 //A game loop just repeats itself to constantly update game render in a manually set interval
 //(eg..updating snake position, food, game calculations (basically, doing repaints)...)
 
-import {snakeSpeed, update as gameUpdate, draw as gameDraw} from './game.js'
+import {snakeSpeed, logic as gameLogic, update as gameUpdate, draw as gameDraw} from './game.js'
 
 
 let lastRenderTime = 0;     //this GLOBAL variable stores the value of currentTime after calling 
 
+const gameBoard = document.getElementById('game-board');
+
 function main(currentTime){ //currentTime represents the 'time' when this function runs requestAnimationFrame() 
-    window.requestAnimationFrame(main);
-    let secondsSinceLastRender= (currentTime - lastRenderTime)/1000;//show the delay in each of those render frames
+    while(!gameOver){
+        if(resume){
+            window.requestAnimationFrame(main);
+            let secondsSinceLastRender= (currentTime - lastRenderTime)/1000;//show the delay in each of those render frames
 
-    // requestAnimationFrame() method tells the browser that you wish
-    // to perform an animation and requests that the browser calls a specified 
-    // function (here, its main()) to update an animation before the next repainting. 
-    // The method takes  a callback as an argument to be invoked before the repaint.
-    if(secondsSinceLastRender < 1 / snakeSpeed){
-        return;
+            // requestAnimationFrame() method tells the browser that you wish
+            // to perform an animation and requests that the browser calls a specified 
+            // function (here, its main()) to update an animation before the next repainting. 
+            // The method takes  a callback as an argument to be invoked before the repaint.
+            if(secondsSinceLastRender < 1 / snakeSpeed){
+                return;
+            }
+            //we go below this line only of the above if statement stands false.
+
+
+
+
+
+
+            console.log("render");
+            
+            lastRenderTime = currentTime;//after executing window.requestAnimationFrame(main) currentTime is stored in lastRender time
+            
+            
+            gameLogic();
+            gameUpdate();
+            gameDraw(gameBoard);
+            //console.log(currentTime);
+            //console.log(secondsSinceLastRender);
+        }
     }
-    //we go below this line only of the above if statement stands false.
-
-
-
-
-
-
-    console.log("render");
-    
-    lastRenderTime = currentTime;//after executing window.requestAnimationFrame(main) currentTime is stored in lastRender time
-    
-    gameUpdate();
-    gameDraw();
-    //console.log(currentTime);
-    //console.log(secondsSinceLastRender);
 }
 
 //entry point
